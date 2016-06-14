bf36e806cf9f08a7bfa312321ff4a159d2c5c472
Rami <rami15manna@gmail.com>
2016-06-14 16:50:08 -0400

    vid placing

--- js/video.js
+++ js/video.js
@@ -14,12 +14,3 @@ function handleVideo(stream) {
 function videoError(e) {
     // do something
 }
-
-// window.requestAnimationFrame ||
-//     (window.requestAnimationFrame = window.webkitRequestAnimationFrame ||
-//         window.mozRequestAnimationFrame ||
-//         window.oRequestAnimationFrame ||
-//         window.msRequestAnimationFrame ||
-//         function( callback ){
-//             window.setTimeout(callback, 1000 / 60);
-//         });
