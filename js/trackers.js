90573e18502f6f221c2e06c1c2ad7beffee0fda2
Rami <rami15manna@gmail.com>
2016-06-23 14:51:57 -0400

    adding backingCanvas

--- js/trackers.js
+++ js/trackers.js
@@ -1,45 +1,49 @@
-  var my_c = document.getElementById("myCanvas");
-  var my_ctx = my_c.getContext("2d");
+  var front = document.getElementById("myCanvas");
+  var front_ctx = front.getContext("2d");
 
   var video = document.getElementById("myVideo");
 
-  var video_c = document.getElementById("videoCanvas");
-  var video_ctx = video_c.getContext("2d");
-  // video_ctx.drawImage(video, 0, 0, 400, 300);
-  // var image_data = video_ctx.getImageData(0, 0, 400, 300);
+  var back = document.getElementById("backingCanvas");
+  var back_context = bc.getContext("2d");
+
+  var canvas = document.getElementById("videoCanvas");
+  var canvas_context = canvas.getContext("2d");
+  // canvas_context.drawImage(video, 0, 0, 400, 300);
+  // var image_data = canvas_context.getImageData(0, 0, 400, 300);
 
 document.addEventListener('DOMContentLoaded', function(){
     // var v = document.getElementById('v');
     // var canvas = document.getElementById('c');
     // var context = canvas.getContext('2d');
 
-    var cw = Math.floor(video_c.clientWidth);
-    var ch = Math.floor(video_c.clientHeight);
-    video_c.width = cw;
-    video_c.height = ch;
+    var cw = Math.floor(canvas.clientWidth);
+    var ch = Math.floor(canvas.clientHeight);
+    canvas.width = cw;
+    canvas.height = ch;
 
     video.addEventListener('playing', function(){
-        draw(this,video_ctx,cw,ch);
+        draw(this,canvas_context,back_context,cw,ch);
     },false);
 
 },false);
 
-function draw(v,ctx,w,h) {
+function draw(v,ctx,b_ctx,w,h) {
     if(v.paused || v.ended) return false;
-    var image_data = video_ctx.getImageData(0, 0, w, h);
-    // var data = image_data.data;
-    // for(var i = 0; i < data.length; i+=4) {
-    //     var r = data[i];
-    //     var g = data[i+1];
-    //     var b = data[i+2];
-    //     var brightness = (3*r+4*g+b)>>>3;
-    //     data[i] = brightness;
-    //     data[i+1] = brightness;
-    //     data[i+2] = brightness;
-    // }
-    // image_data.data = data;
+    b_ctx.drawImage(v,0,0,w,h);
+    var image_data = b_ctx.getImageData(0, 0, w, h);
+    var data = image_data.data;
+    for(var i = 0; i < data.length; i+=4) {
+        var r = data[i];
+        var g = data[i+1];
+        var b = data[i+2];
+        var brightness = (3*r+4*g+b)>>>3;
+        data[i] = brightness;
+        data[i+1] = brightness;
+        data[i+2] = brightness;
+    }
+    image_data.data = data;
     ctx.putImageData(image_data,0,0,0,0,w,h);
-    setTimeout(draw,20,v,ctx,w,h);
+    setTimeout(draw,20,v,ctx,b_ctx,w,h);
 }
 
   // var tacking_canvas = new Canvas();
@@ -57,8 +61,8 @@ function draw(v,ctx,w,h) {
   //     });
   //   }
   // });
-  my_ctx.lineWidth="1";
-  my_ctx.strokeStyle="purple";
+  front_ctx.lineWidth="1";
+  front_ctx.strokeStyle="purple";
   var face_tracker = new tracking.ObjectTracker("face");
 
   face_tracker.setInitialScale(4);
@@ -66,14 +70,14 @@ function draw(v,ctx,w,h) {
   face_tracker.setEdgesDensity(0.1);
 
   face_tracker.on('track',function(event){
-    my_ctx.clearRect(0,0,my_c.width,my_c.height);
+    front_ctx.clearRect(0,0,front.width,front.height);
     if(event.data.length === 0) {
     } else {
       event.data.forEach(function(rect){
-        my_ctx.clearRect(0,0,my_c.width,my_c.height);
-        my_ctx.beginPath();
-        my_ctx.strokeRect(rect.x,rect.y,rect.width,rect.height);
-        my_ctx.stroke();        
+        front_ctx.clearRect(0,0,front.width,front.height);
+        front_ctx.beginPath();
+        front_ctx.strokeRect(rect.x,rect.y,rect.width,rect.height);
+        front_ctx.stroke();        
       });
     }
   });
