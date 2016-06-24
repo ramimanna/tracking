826dc62dc4afa382ff24550024e6eefd4046cc04
Rami <rami15manna@gmail.com>
2016-06-24 12:54:07 -0400

    debug

--- js/trackers.js
+++ js/trackers.js
@@ -47,7 +47,7 @@ function draw(v,ctx,b_ctx,w,h) {
     
     //GRAYSCALE 
     if(filter == "grayscale"){
-      tracking.Image.grayscale(image_data.data, w, h, true);
+      tracking.Image.grayscale(image_data.data, w, h, false);
       console.log(image_data.data[0]);
       
       // jsfeat.imgproc.grayscale(image_data.data, w, h, img_u8);
